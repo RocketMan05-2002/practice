b7dc0d1453ae7d959eeacade0160d8100cf2cd15
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-04 18:08:33 +0530

    star rating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Accordian from './components/01_accordian/inde'
 import RandomColor from './components/02_random_color_generator'
+import StarRating from './components/03_star_rating'
 
 const App = () => {
   return (
     <div>
         {/* <Accordian /> */}
         {/* <RandomColor /> */}
-        
+        <StarRating noOfStars={10}/>
     </div>
   )
 }
