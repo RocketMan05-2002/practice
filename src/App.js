45bbbc34b8781a881c064a1b7eec6e616340c349
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-04 17:05:14 +0530

    accordian and random color generator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Accordian from './components/01_accordian/inde'
+import RandomColor from './components/02_random_color_generator'
+
+const App = () => {
+  return (
+    <div>
+        {/* <Accordian /> */}
+        {/* <RandomColor /> */}
+        
+    </div>
+  )
+}
+
+export default App
