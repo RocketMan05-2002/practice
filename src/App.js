e65095c4b1c71974c2dc97754f3e998da14935ec
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-11 23:29:18 +0530

    format fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import './App.css'
-import Accordian from './components/01_accordian/inde'
-import RandomColor from './components/02_random_color_generator'
-import StarRating from './components/03_star_rating'
-import ImageSlider from './components/04_image_slider/index'
-import LoadMore from './components/05_load_more_button'
-import QRCodeGenerator from './components/07_qr_code_generator'
-import ItemsManager from './containers/01_items_manager'
+import React from "react";
+import "./App.css";
+import Accordian from "./components/01_accordian/inde";
+import RandomColor from "./components/02_random_color_generator";
+import StarRating from "./components/03_star_rating";
+import ImageSlider from "./components/04_image_slider/index";
+import LoadMore from "./components/05_load_more_button";
+import QRCodeGenerator from "./components/07_qr_code_generator";
+import ItemsManager from "./containers/01_items_manager";
 
 const App = () => {
   return (
     <div>
-        {/* <Accordian /> */}
-        {/* <RandomColor /> */}
-        {/* <StarRating noOfStars={10}/> */}
-        {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={1} /> */}
-        {/* <LoadMore /> */}
-        {/* <QRCodeGenerator /> */}
-        <ItemsManager />
+      {/* <Accordian /> */}
+      {/* <RandomColor /> */}
+      {/* <StarRating noOfStars={10}/> */}
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={1} /> */}
+      {/* <LoadMore /> */}
+      {/* <QRCodeGenerator /> */}
+      <ItemsManager />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/05_load_more_button/index.js
+++ src/components/05_load_more_button/index.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import './load.css'
+import "./load.css";
 
 export default function LoadMore() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
-  const [ disableButton,setDisabelButton] = useState(false);
+  const [disableButton, setDisabelButton] = useState(false);
 
   async function fetchProducts() {
     try {
@@ -17,12 +17,12 @@ export default function LoadMore() {
       );
       const data = await response.json();
       console.log(data);
-      if(data && data.products && data.products.length){
-        setProducts((prevData)=>[...prevData,...data.products]);
+      if (data && data.products && data.products.length) {
+        setProducts((prevData) => [...prevData, ...data.products]);
         setLoading(false);
       }
     } catch (e) {
-        setLoading(false);
+      setLoading(false);
       console.log(e.message);
     }
   }
@@ -31,28 +31,29 @@ export default function LoadMore() {
     fetchProducts();
   }, [count]);
 
-  useEffect(()=>{
-    if(products&&products.length===100) setDisabelButton(true);
-  },[products])
+  useEffect(() => {
+    if (products && products.length === 100) setDisabelButton(true);
+  }, [products]);
 
-  if(loading) <div>Loading data...please wait</div>
+  if (loading) <div>Loading data...please wait</div>;
 
-  return <div className="load-more-container">
-    <div className="product-container">
-        {
-            products && products.length ? 
-            products.map(item=><div key={item.id} className="product-item">
-                <img  
-                src={item.thumbnail}
-                alt={item.title}
-                />
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div key={item.id} className="product-item">
+                <img src={item.thumbnail} alt={item.title} />
                 <p>{item.title}</p>
-            </div>)
-            :null
-        }
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button onClick={() => setCount(count + 1)} disabled={disableButton}>
+          Load More Prodcuts
+        </button>
+      </div>
     </div>
-    <div className="button-container">
-        <button onClick={()=>setCount(count+1)} disabled={disableButton}>Load More Prodcuts</button>
-    </div>
-  </div>;
+  );
 }
