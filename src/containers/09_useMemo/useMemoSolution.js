fb719e159905ab7e0ad750f03b2ea2f69252a711
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-23 23:58:38 +0530

    items manager styling and UI update

--- src/containers/09_useMemo/useMemoSolution.js
+++ src/containers/09_useMemo/useMemoSolution.js
@@ -1,28 +1,32 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useState } from "react";
 
-export default function UseMemoExp(){
-    const[count,setCount] = useState(0);
-    const[input,setInput] = useState(0);
+export default function UseMemoExp() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
 
-    function expensiveTask(num){
-        console.log("inside expensivce task");
-        for(let i=0;i<=1000000000;i++){}
-        return num*2;
-    }
+  function expensiveTask(num) {
+    console.log("inside expensivce task");
+    for (let i = 0; i <= 1000000000; i++) {}
+    return num * 2;
+  }
 
-    let doubleValue = useMemo(()=> expensiveTask(input),[input]);
+  let doubleValue = useMemo(() => expensiveTask(input), [input]);
 
-    //1st param is a function which will be rendered only on dependency change
-    //if input doest change function won't render unnecessarily again.
-    // only the LAST input value calculation is stored.
+  //1st param is a function which will be rendered only on dependency change
+  //if input doest change function won't render unnecessarily again.
+  // only the LAST input value calculation is stored.
 
-    return(
-        <div>
-            <button onClick={()=>setCount(count+1)} >Increment</button>
-            {count}
-            <input type='number' placeholder='enter number to double up' value={input}
-            onChange={(e)=>setInput(e.target.value)} />
-            <div>Double: { doubleValue } </div>
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      {count}
+      <input
+        type="number"
+        placeholder="enter number to double up"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <div>Double: {doubleValue} </div>
+    </div>
+  );
 }
