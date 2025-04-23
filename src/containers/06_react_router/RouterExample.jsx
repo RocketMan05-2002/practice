eaf0a7ea8f407c3cdeb256e7323181efdfc582ee
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-24 00:00:02 +0530

    format fixes

--- src/containers/06_react_router/RouterExample.jsx
+++ src/containers/06_react_router/RouterExample.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 const RouterExample = () => {
   return (
     <BrowserRouter>
-        <Navbar />
-        <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
-            <Route path='/about' element={<About />}></Route>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default RouterExample
+export default RouterExample;
