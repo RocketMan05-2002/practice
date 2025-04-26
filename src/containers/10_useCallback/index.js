d781e9ff96a4d6d6475b5efd3c31573d2f8c535d
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-26 23:59:26 +0530

    practice and useReducer fetch with additional features

--- src/containers/10_useCallback/index.js
+++ src/containers/10_useCallback/index.js
@@ -1,20 +1,19 @@
-import { useCallback, useState } from "react"
+import { useCallback, useState } from "react";
 import ChildComp from "./ChildComp";
 
+export default function UseContextExp() {
+  const [count, setCount] = useState(0);
 
-export default function UseContextExp(){
-    const[count,setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
 
-    const handleClick = useCallback(() =>{
-            setCount(count+1);
-    },[count]);
-
-    return(
-        <div>
-            Count: {count}
-            <br/>
-            <button onClick={handleClick}>Increment</button> <br/>
-            <ChildComp buttonName={"Clickme"} handleClick={handleClick}/>
-        </div>
-    )
+  return (
+    <div>
+      Count: {count}
+      <br />
+      <button onClick={handleClick}>Increment</button> <br />
+      <ChildComp buttonName={"Clickme"} handleClick={handleClick} />
+    </div>
+  );
 }
