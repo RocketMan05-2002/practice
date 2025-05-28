19698640cacbc40966da726b8c58c5549005341a
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-28 23:57:05 +0530

    format

--- src/components/01_accordian/inde.js
+++ src/components/01_accordian/inde.js
@@ -1,58 +1,67 @@
-import { useState } from "react"
-import data from './data'
-import './accordian.css'
+import { useState } from "react";
+import data from "./data";
+import "./accordian.css";
 
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-export default function Accordian(){
+  function handleSingleSelection(id) {
+    setSelected(id === selected ? null : id);
+  }
 
-    const[selected,setSelected] = useState(null);
-    const[enableMultiSelection,setEnableMultiSelection] = useState(false);
-    const[multiple,setMultiple] = useState([]);
+  function handleMultiSelection(id) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfId = cpyMultiple.indexOf(id);
+    if (findIndexOfId === -1) cpyMultiple.push(id);
+    else cpyMultiple.splice(findIndexOfId, 1);
+    setMultiple(cpyMultiple);
+    console.log(cpyMultiple);
+  }
 
-    function handleSingleSelection(id){
-        setSelected(id===selected?null:id);
-    }
-
-    function handleMultiSelection(id){
-        let cpyMultiple = [...multiple];
-        const findIndexOfId = cpyMultiple.indexOf(id);
-        if(findIndexOfId === -1) cpyMultiple.push(id)
-        else cpyMultiple.splice(findIndexOfId,1);
-        setMultiple(cpyMultiple);
-        console.log(cpyMultiple);
-    }
-
-    return <div className="accordian__wrapper">
-        <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)}>
-            {enableMultiSelection?"Enable Single Selection":"Enable Multi Selection"}
-        </button>
-        <div className="accordian">
-            {
-                data && data.length >0 ? 
-                data.map(item=><div className="accordian__item" key={item.id}>
-                    <div className="accordian__title" onClick={
-                        enableMultiSelection 
-                        ? () => handleMultiSelection(item.id)
-                        : () => handleSingleSelection(item.id)
-                    } >
-                        <h3>{item.question}</h3>
-                        <span>+</span>
-                    </div>
-                    {
-                        enableMultiSelection ?
-                        multiple.indexOf(item.id)!==-1 && <div className="accordian__content">{item.answer}</div>
-                        : selected===item.id && <div className="accordian__content">{item.answer}</div>
-                    }
-                    {/* {
+  return (
+    <div className="accordian__wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {enableMultiSelection
+          ? "Enable Single Selection"
+          : "Enable Multi Selection"}
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((item) => (
+            <div className="accordian__item" key={item.id}>
+              <div
+                className="accordian__title"
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(item.id)
+                    : () => handleSingleSelection(item.id)
+                }
+              >
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(item.id) !== -1 && (
+                    <div className="accordian__content">{item.answer}</div>
+                  )
+                : selected === item.id && (
+                    <div className="accordian__content">{item.answer}</div>
+                  )}
+              {/* {
                         selected === item.id || multiple.indexOf(item.id)!==-1  ? 
                         <div className="accordian__content">
                             {item.answer}
                         </div>
                         :null
                     } */}
-                </div>)
-                :<div>No Data Found!</div>
-            }
-        </div>
+            </div>
+          ))
+        ) : (
+          <div>No Data Found!</div>
+        )}
+      </div>
     </div>
+  );
 }
