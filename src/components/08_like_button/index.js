d02de81c9dc74578efeec3fa73c50b7bb7e934ce
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-26 18:12:59 +0530

    likeBtn linkedin and instagram

--- src/components/08_like_button/index.js
+++ src/components/08_like_button/index.js
@@ -1,17 +1,32 @@
-import "./like.css";
-import { BsHeart } from "react-icons/bs";
-import { useState } from "react";
+import { useState } from "react"
+import { BsHeart, BsLinkedin, BsInstagram } from "react-icons/bs" 
+import "./like.css"
 
-export default function Like() {
-  const [liked, setLiked] = useState(false);
-  return (
-    <div className="App">
-      <button
-        className={`likeBtn ${liked ? "liked" : ""}`}
-        onClick={() => setLiked(!liked)}
-      >
-        <BsHeart /> {liked ? "liked" : "like"}
-      </button>
+export default function Like(){
+    const[liked,setLiked] = useState(false);
+    const[social,setSocial] = useState(false);
+    const[instaed, setInstaed] = useState(false);
+    return <div className="wrapper">
+        <button
+        className={`likeBtn ${liked?"liked":""}`}
+        onClick={()=>setLiked(!liked)}
+        >
+            <BsHeart size={25} />
+            <span>{liked?"liked":"like"}</span>
+        </button>
+        <button
+        className={`linkBtn ${social?"linked":""}`}
+        onClick={()=>setSocial(!social)}
+        >
+            <BsLinkedin size={25} />
+            <span>{social?"Linked":"Link"}</span>
+        </button>
+        <button
+        className={`instaBtn ${instaed?"instaed":""}`}
+        onClick={()=>setInstaed(!instaed)}
+        >
+            <BsInstagram size={25} />
+            <span>{instaed?"insta-ed":"insta"}</span>
+        </button>
     </div>
-  );
 }
