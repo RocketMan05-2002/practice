aede1b7961cfd6a175a6068d192eb74252d73638
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-27 13:23:53 +0530

    btns play modification

--- src/components/08_like_button/index.js
+++ src/components/08_like_button/index.js
@@ -1,34 +1,48 @@
-import { useState } from "react";
-import { BsHeart, BsLinkedin, BsInstagram } from "react-icons/bs";
-import "./like.css";
+import { useState } from "react"
+import "./like.css"
+import { BsHeartFill, BsLinkedin, BsInstagram, BsFacebook } from "react-icons/bs"
+import { FaThumbsUp  } from "react-icons/fa"
 
-export default function Like() {
-  const [liked, setLiked] = useState(false);
-  const [social, setSocial] = useState(false);
-  const [instaed, setInstaed] = useState(false);
-  return (
-    <div className="wrapper">
-      <button
-        className={`likeBtn ${liked ? "liked" : ""}`}
-        onClick={() => setLiked(!liked)}
-      >
-        <BsHeart size={25} />
-        <span>{liked ? "liked" : "like"}</span>
-      </button>
-      <button
-        className={`linkBtn ${social ? "linked" : ""}`}
-        onClick={() => setSocial(!social)}
-      >
-        <BsLinkedin size={25} />
-        <span>{social ? "Linked" : "Link"}</span>
-      </button>
-      <button
-        className={`instaBtn ${instaed ? "instaed" : ""}`}
-        onClick={() => setInstaed(!instaed)}
-      >
-        <BsInstagram size={25} />
-        <span>{instaed ? "insta-ed" : "insta"}</span>
-      </button>
+export default function Practice(){
+
+    const [states,setStates] = useState({
+        like: false,
+        love: false,
+        fb: false,
+        lin:false,
+        insta:false
+    })
+
+    return <div className="wrapper">
+        <button
+        onClick={()=>setStates({...states, like:true})}
+        className={`btn ${states.like?"liked":""}`}
+        >
+            <FaThumbsUp size={25}/> <span> {states.like?"liked":"like"} </span> 
+        </button>
+        <button
+        onClick={()=>setStates({...states, love:true})}
+        className={`btn ${states.love?"loved":""}`}
+        >
+            <BsHeartFill size={25} /> <span>{states.love?"loved":"love"} </span> 
+        </button>
+        <button
+        onClick={()=>setStates({...states, fb:true})}
+        className={`btn ${states.fb?"fbed":""}`}
+        >
+            <BsLinkedin size={25} /> <span>{states.fb?"connected":"connect"}</span> 
+        </button>
+        <button
+        onClick={()=>setStates({...states, lin:true})}
+        className={`btn ${states.lin?"lined":""}`}
+        >
+            <BsFacebook size={25} /> <span>{states.lin?"connected":"connect"}</span> 
+        </button>
+        <button
+        onClick={()=>setStates({...states, insta:true})}
+        className={`btn ${states.insta?"instaed":""}`}
+        >
+            <BsInstagram size={25} /> <span>{states.insta?"connected":"connect"}</span> 
+        </button>
     </div>
-  );
 }
