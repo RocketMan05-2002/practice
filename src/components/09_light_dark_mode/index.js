f40e9eb4210abc83cc1cb6f94b419e6022b19d9b
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-27 23:54:52 +0530

    scroll indicator project add

--- src/components/09_light_dark_mode/index.js
+++ src/components/09_light_dark_mode/index.js
@@ -1,20 +1,21 @@
-import useLocalStorage from "./hooks/useLocalStorage"
-import "./theme.css"
+import useLocalStorage from "./hooks/useLocalStorage";
+import "./theme.css";
 
-export default function Practice(){
+export default function Practice() {
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
 
-    const[theme,setTheme] = useLocalStorage('theme','dark');
+  function handleThemeToggle() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
 
-    function handleThemeToggle(){
-        setTheme(theme==="light"?"dark":"light");
-    }
+  console.log(theme);
 
-    console.log(theme);
-
-    return <div className="light-dark-mode" data-theme={theme}>
-        <div className="container" >
-            <p>{theme==="light"?"Black lives matter":"White lives matter"}</p>
-            <button onClick={handleThemeToggle} >Change Theme</button>
-        </div>
+  return (
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <p>{theme === "light" ? "Black lives matter" : "White lives matter"}</p>
+        <button onClick={handleThemeToggle}>Change Theme</button>
+      </div>
     </div>
+  );
 }
