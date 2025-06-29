74d973bd61f8a32bc2b5ed26ec1e6e4f8054f8ad
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-29 23:55:24 +0530

    add custom modal popup component

--- src/components/12_custom_modal_popup/Modal.jsx
+++ src/components/12_custom_modal_popup/Modal.jsx
@@ -1,16 +1,14 @@
-
-
-export default function Modal({id,header,body,footer,setShowModal}){
-    return <div id={id || "Modal"} className="modal">
-            <div className="header">
-                <span onClick={()=>setShowModal(false)} >&times;</span>
-                <h2>{header ? header : "Header"}</h2>
-            </div>
-            <div className="body">
-                { body? body : "Body" }
-            </div>
-            <div className="footer">
-                <h2>{footer?footer:"Footer"}</h2>
-            </div>
+export default function Modal({ id, header, body, footer, setShowModal }) {
+  return (
+    <div id={id || "Modal"} className="modal">
+      <div className="header">
+        <span onClick={() => setShowModal(false)}>&times;</span>
+        <h2>{header ? header : "Header"}</h2>
+      </div>
+      <div className="body">{body ? body : "Body"}</div>
+      <div className="footer">
+        <h2>{footer ? footer : "Footer"}</h2>
+      </div>
     </div>
+  );
 }
