6435e8d6aab6645cd229ee2bf438b6ad6f786a4f
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-29 23:54:54 +0530

    add custom modal popup component

--- src/components/12_custom_modal_popup/index.js
+++ src/components/12_custom_modal_popup/index.js
@@ -1,21 +1,21 @@
 import { useState } from "react";
-import Modal from "./Modal"
-import "./modal.css"
+import Modal from "./Modal";
+import "./modal.css";
 
-export default function Modaltest(){
-    const[showModal,setShowModal] = useState(false);
-    return <div className="container">
-        <button  onClick={()=>setShowModal(!showModal)} >Open modal popup</button>
-        {
-            showModal && (
-                <Modal 
-                id={"custom-id"}
-                header={<h1>Custom header</h1>}
-                body={<h1>Custom body</h1>}
-                footer={<h1>Custom footer</h1>}
-                setShowModal={setShowModal}
-                />
-            )
-        }
+export default function Modaltest() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div className="container">
+      <button onClick={() => setShowModal(!showModal)}>Open modal popup</button>
+      {showModal && (
+        <Modal
+          id={"custom-id"}
+          header={<h1>Custom header</h1>}
+          body={<h1>Custom body</h1>}
+          footer={<h1>Custom footer</h1>}
+          setShowModal={setShowModal}
+        />
+      )}
     </div>
+  );
 }
