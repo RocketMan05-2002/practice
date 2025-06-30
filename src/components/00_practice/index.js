ab870861ad529dc6b03bedb8f4029a7cd660d612
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-30 23:51:15 +0530

    add custom modal popup component

--- src/components/00_practice/index.js
+++ src/components/00_practice/index.js
@@ -1,7 +1,3 @@
-
-
-export default function Practice(){
-    return <div className="github-profile-finder">
-        
-    </div>
+export default function Practice() {
+  return <div className="github-profile-finder"></div>;
 }
